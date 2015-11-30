eacc5586f6d53a7982b82c6cfd8027595d0804f6
Xaxetrov <edern.haumont@orange.fr>
2015-11-30 15:39:19 +0100

    checked main

--- main.cpp
+++ main.cpp
@@ -17,34 +17,34 @@ int main(int argc, char * argv[])
     int tHour = 0;
     string gFilePath = "";
     string logFilePath = "";
-    int currantArg = 1; //pass the first arg which is the programe name (+ path)
+    int currentArg = 1; //pass the first arg which is the program name (+ path)
 
     //read the input arguments:
 
-    while (currantArg < argc-1)
+    while (currentArg < argc-1)
     {
-        if(argv[currantArg][0] == '-')
+        if(argv[currentArg][0] == '-')
         {
-           switch (argv[currantArg][1])
+           switch (argv[currentArg][1])
            {
                case 'e':
                    optionE = true;
                    break;
                case 't':
                    optionT = true;
-                   currantArg++; //if the argument is "-t" then the next must be the hour
-                   tHour = atoi(argv[currantArg]);
+                   currentArg++; //if the argument is "-t" then the next must be the hour
+                   tHour = atoi(argv[currentArg]);
                    break;
                case 'g':
                    optionG = true;
-                   currantArg++; //if the argument is "-g" then the next must be the file path
-                   gFilePath = argv[currantArg];
+                   currentArg++; //if the argument is "-g" then the next must be the file path
+                   gFilePath = argv[currentArg];
                    break;
            }
         }
-        currantArg++;
+        currentArg++;
     }
-    if(currantArg >= argc)
+    if(currentArg >= argc)
     {
         cout << "wrong number of arguments" << endl;
         return 1;
@@ -52,8 +52,6 @@ int main(int argc, char * argv[])
     logFilePath = argv[argc-1];
 
     manager.LoadLogFile(logFilePath);
-
-
 
     return 0;
 }
